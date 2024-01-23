b10e8775420074203aa0e14402a65e9cf088df8f
CrioUser <crio-user@criodo.com>
2024-01-24 03:34:00 +0530

    “ME_QKART_FRONTEND_V2_MODULE_REGISTER”

--- src/components/Register.js
+++ src/components/Register.js
@@ -8,6 +8,7 @@ import Footer from "./Footer";
 import Header from "./Header";
 import "./Register.css";
 
+
 const Register = () => {
   const { enqueueSnackbar } = useSnackbar();
 
@@ -35,7 +36,67 @@ const Register = () => {
    *      "message": "Username is already taken"
    * }
    */
+
+
+   const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+
+  });
+  
+  const [isLoading, setIsLoading] = useState(false);
+
+
+
+
   const register = async (formData) => {
+
+    setIsLoading(true)
+
+    try {
+      // if (!validateInput()) {
+      //   return;
+      // }
+
+      const apiUrl = `${config.endpoint}/auth/register`;
+      const response = await axios.post(apiUrl, {
+        username: formData.username,
+        password: formData.password,
+      });
+
+
+
+
+
+      if (response.status === 201) {
+        // Registration successful
+        enqueueSnackbar("Registered Successfully", { variant: "success" });
+        // Additional logic, if needed
+      } 
+      setIsLoading(false)
+
+      // else {
+      //   // Handle unexpected status codes
+      //   enqueueSnackbar("Something went wrong. Please try again.", {
+      //     variant: "error",
+      //   });
+      // }
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        // Registration failed due to validation errors
+        enqueueSnackbar(error.response.data.message, { variant: "error" });
+      } else {
+        // Other errors (network issues, server errors, etc.)
+        enqueueSnackbar(
+          "Something went wrong. Check that the backend is running, reachable, and returns valid JSON.",
+          { variant: "error" }
+        );
+      }
+
+      setIsLoading(false)
+    }
+
   };
 
   // TODO: CRIO_TASK_MODULE_REGISTER - Implement user input validation logic
@@ -57,7 +118,40 @@ const Register = () => {
    * -    Check that confirmPassword field has the same value as password field - Passwords do not match
    */
   const validateInput = (data) => {
+
+
+    if (!data.username) {
+      enqueueSnackbar("Username is a required field", { variant: "warning" });
+      return false;
+    } else if (data.username.length < 6) {
+      enqueueSnackbar("Username must be at least 6 characters", {
+        variant: "warning",
+      });
+      return false;
+    } else if (!data.password) {
+      enqueueSnackbar("Password is a required field", { variant: "warning" });
+      return false;
+    } else if (data.password.length < 6) {
+      enqueueSnackbar("Password must be at least 6 characters", {
+        variant: "warning",
+      });
+      return false;
+    } else if (data.password !== data.confirmPassword) {
+      enqueueSnackbar("Passwords do not match", { variant: "warning" });
+      return false;
+    } else {
+      return true;
+    }
+
+
+
   };
+
+
+
+
+
+
 
   return (
     <Box
@@ -78,6 +172,11 @@ const Register = () => {
             name="username"
             placeholder="Enter Username"
             fullWidth
+
+            onChange={(e) =>
+              setFormData({ ...formData, username: e.target.value })
+            }
+
           />
           <TextField
             id="password"
@@ -88,6 +187,11 @@ const Register = () => {
             helperText="Password must be atleast 6 characters length"
             fullWidth
             placeholder="Enter a password with minimum 6 characters"
+
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
+
           />
           <TextField
             id="confirmPassword"
@@ -96,10 +200,28 @@ const Register = () => {
             name="confirmPassword"
             type="password"
             fullWidth
+
+            onChange={(e) =>
+              setFormData({ ...formData, confirmPassword: e.target.value })
+            }
+
           />
-           <Button className="button" variant="contained">
+          {isLoading ? (
+            <Box display="flex" justifyContent="center">
+            <CircularProgress />
+          </Box>
+          ) : (
+
+            <Button className="button" variant="contained"onClick={() => validateInput(formData) && register(formData)}>
             Register Now
            </Button>
+            
+          )}
+
+          
+           
+
+
           <p className="secondary-action">
             Already have an account?{" "}
              <a className="link" href="#">
